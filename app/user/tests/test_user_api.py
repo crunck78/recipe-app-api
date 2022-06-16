738041c985112b8ee8810660b5f2898e8c91b09a
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2022-06-16 22:23:59 +0200

    fix linting and typo erros

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -1,7 +1,6 @@
 """
 Tests for the user API.
 """
-import email
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -33,7 +32,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        user = get_user_model().objects.get(email=payload('email'))
+        user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
@@ -44,7 +43,7 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass123',
             'name': 'Test Name',
         }
-        user = create_user(**payload)
+        create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
@@ -56,7 +55,9 @@ class PublicUserApiTests(TestCase):
             'password': 'pw',
             'name': 'Test Name',
         }
-        res = self.client.post(CREATE_USER_URL, status.HTTP_400_BAD_REQUEST)
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
