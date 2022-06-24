30927f95fef5057039c6546d6e03513b34af4600
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2022-06-24 23:16:20 +0200

    Modify recipe model to handle storing images

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,7 +20,7 @@ from drf_spectacular.views import (
 
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.url.static import static
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
@@ -35,7 +35,7 @@ urlpatterns = [
     path('api/recipe/', include('recipe.urls')),
 ]
 
-if setting.DEBUG:
+if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
